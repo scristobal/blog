7d57d0ed626f145aed87a643f86e218a6dfdd178
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-11-10 12:54:25 +0100

    Change code theme

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -2,5 +2,5 @@ import { defineMarkdocConfig } from "@astrojs/markdoc/config";
 import shiki from "@astrojs/markdoc/shiki";
 
 export default defineMarkdocConfig({
-  extends: [shiki()],
+  extends: [shiki({theme: "ayu-dark"})],
 });
