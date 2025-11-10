a974f64914564ae1165e881a3957a4d6243a74d9
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-11-10 12:32:57 +0100

    Move content to markdoc

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import mdx from "@astrojs/mdx";
+import markdoc from "@astrojs/markdoc";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
@@ -6,7 +6,7 @@ import { SITE_URL } from "./src/consts";
 
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap()],
+  integrations: [markdoc(), sitemap()],
   prefetch: true,
   vite: {
     server: { open: true },
